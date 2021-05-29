0aa9ff6123df9316aa81259e08a12444cb154757
Ashish Bajaj <bajajashu97@gmail.com>
2021-05-29 19:08:51 +0530

    sad gitignore

--- dist/config.js
+++ dist/config.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TWILIO_ACCOUNT_AUTH = exports.TWILIO_ACCOUNT_SID = void 0;
 exports.TWILIO_ACCOUNT_SID = "AC93b682237493db0d43f4bbef9bbac0bc";
-exports.TWILIO_ACCOUNT_AUTH = "cf076343cfaea9bc2ffca5bf17752860";
+exports.TWILIO_ACCOUNT_AUTH = "a7c1e7b6cde563e5639861b9b1aa9cce";
