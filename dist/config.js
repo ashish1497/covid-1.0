aff7146c743c0a6cf7b72b4ee8c920a6c936045f
Ashish Bajaj <bajajashu97@gmail.com>
2021-05-29 19:11:36 +0530

    did it work

--- dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TWILIO_ACCOUNT_AUTH = exports.TWILIO_ACCOUNT_SID = void 0;
-exports.TWILIO_ACCOUNT_SID = "AC93b682237493db0d43f4bbef9bbac0bc";
-exports.TWILIO_ACCOUNT_AUTH = "a7c1e7b6cde563e5639861b9b1aa9cce";
